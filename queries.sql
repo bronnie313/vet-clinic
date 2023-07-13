9124caab55355ff2ace57ce8d82c3aa3ce3fc25b
Bisaso Ronald <bronnie313@gmail.com>
2023-07-13 18:12:13 +0300

    add alter table command to the queries file

--- queries.sql
+++ queries.sql
@@ -7,6 +7,7 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+ALTER TABLE animals ADD COLUMN species VARCHAR(255);
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
