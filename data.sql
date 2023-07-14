e76518bb33b726344e1a0344068a729c1e07f2a3
Bisaso Ronald <bronnie313@gmail.com>
2023-07-14 13:46:26 +0300

    add species to the species table

--- data.sql
+++ data.sql
@@ -13,3 +13,5 @@ INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
 INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
 INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
