1514e422eb0360577f272ee11400e6ef56c6728a
Bisaso Ronald <bronnie313@gmail.com>
2023-07-15 22:58:58 +0300

    add many to many relationship to the  vet_clinic database

--- queries.sql
+++ queries.sql
@@ -40,6 +40,73 @@ SELECT animals.name FROM animals JOIN owners ON animals.owner_id = owners.id WHE
 SELECT owners.full_name FROM owners
 JOIN animals ON owners.id = animals.owner_id 
 GROUP BY owners.full_name ORDER BY COUNT(*) DESC LIMIT 1;
+SELECT * FROM animals JOIN visits ON animals.id = visits.animals_id 
+JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'William Tatcher'
+ORDER BY visits.visit_date DESC
+LIMIT 1;
+
+SELECT COUNT(DISTINCT visits.animals_id)
+FROM visits 
+JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'Stephanie Mendez';
+
+SELECT v.name AS vet_name, s.name AS specialty_name
+FROM vets v
+LEFT JOIN specializations sp ON v.id = sp.vet_id
+LEFT JOIN species s ON sp.species_id = s.id
+ORDER BY v.name;
 
 
+SELECT *
+FROM animals 
+JOIN visits ON animals.id = visits.animal_id
+JOIN vets ON visits.vet_id = vets.id
+WHERE vets.name = 'Stephanie Mendez'
+AND visits.visit_date >= '2020-04-01'
+AND visits.visit_date <= '2020-08-30';
 
+SELECT a.name AS animal_name, COUNT(*) AS visit_count
+FROM animals a
+JOIN visits v ON a.id = v.animal_id
+GROUP BY a.name
+ORDER BY visit_count DESC
+LIMIT 1;
+
+SELECT v.name AS vet_name, MIN(vs.visit_date) AS first_visit_date
+FROM visits vs
+JOIN vets v ON v.id = vs.vet_id
+JOIN animals a ON a.id = vs.animals_id
+WHERE a.name = 'Maisy Smith'
+GROUP BY v.name
+ORDER BY first_visit_date ASC
+LIMIT 1;
+
+
+SELECT a.*, v.name AS vet_name, v.visit_date AS visit_date
+FROM visits v
+JOIN animals a ON a.id = v.animal_id
+WHERE v.visit_date = (
+  SELECT MAX(visit_date)
+  FROM visits
+)
+LIMIT 1;
+
+SELECT COUNT(*) AS num_visits
+FROM visits v
+JOIN vets vt ON v.vet_id = vt.id
+JOIN animals a ON v.animals_id = a.id
+LEFT JOIN specializations s ON vt.id = s.vet_id AND a.species_id = s.species_id
+WHERE s.vet_id IS NULL;
+
+
+SELECT s.name AS specialty_name, COUNT(*) AS visit_count
+FROM visits v
+JOIN animals a ON v.animals_id = a.id
+JOIN specializations sp ON a.species_id = sp.species_id
+JOIN vets vt ON sp.vet_id = vt.id
+JOIN species s ON sp.species_id = s.id
+WHERE vt.name = 'Maisy Smith'
+GROUP BY s.name
+ORDER BY visit_count DESC
+LIMIT 1;
